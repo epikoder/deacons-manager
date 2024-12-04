377486907dd31777dc55078d432ac585eb88bad4
epikoder <efedua.bell@gmail.com>
2024-12-04 17:13:20 +0100

    enable https

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import vike from "vike/plugin";
+import fs from "fs";
+
+let cert: string | undefined;
+let key: string | undefined;
+if (fs.existsSync("./certs/cert.pem")) {
+  cert = fs.readFileSync("./certs/cert.pem", "utf8");
+  key = fs.readFileSync("./certs/private.pem", "utf8");
+}
 
 export default defineConfig({
   plugins: [vike({}), react({})],
+  server: cert
+    ? {
+      https: {
+        cert,
+        key,
+      },
+    }
+    : undefined,
 });
