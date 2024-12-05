848bd89c7b4fef352c81ef6eacf1173a00062409
epikoder <efedua.bell@gmail.com>
2024-12-05 21:53:48 +0100

    Proxy: Added rule

--- vite.config.ts
+++ vite.config.ts
@@ -5,19 +5,29 @@ import fs from "fs";
 
 let cert: string | undefined;
 let key: string | undefined;
-if (fs.existsSync("./certs/cert.pem")) {
+if (
+  fs.existsSync("./certs/cert.pem") && process.env.NODE_ENV != "development"
+) {
   cert = fs.readFileSync("./certs/cert.pem", "utf8");
   key = fs.readFileSync("./certs/private.pem", "utf8");
 }
 
+let pgrestURL = process.env.PG_REST ?? "http://localhost:3001";
+
 export default defineConfig({
   plugins: [vike({}), react({})],
-  server: cert
-    ? {
-      https: {
+  server: {
+    https: cert
+      ? {
         cert,
         key,
+      }
+      : undefined,
+    proxy: {
+      "/rest": {
+        target: pgrestURL,
+        rewrite: (path) => path.replace("/rest", ""),
       },
-    }
-    : undefined,
+    },
+  },
 });
