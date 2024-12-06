61ce69eb938900d02c06783d732605570c41cfec
epikoder <efedua.bell@gmail.com>
2024-12-06 09:55:35 +0100

    config: POSTGRES_URI

--- pages/credentials.shared.ts
+++ pages/credentials.shared.ts
@@ -1,2 +1,4 @@
-const POSTGRES_URI = "http://localhost:3000/rest";
+const POSTGRES_URI = process.env.NODE_ENV == "development"
+    ? "http://localhost:3000/rest"
+    : "https://admin.prep50.ng/rest";
 export { POSTGRES_URI };
