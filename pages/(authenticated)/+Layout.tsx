b9cd7c8c5b28e479246d5a7fc32e89973766a3ed
epikoder <efedua.bell@gmail.com>
2025-05-19 15:10:45 +0100

    Added nanyalove and winbest source

--- pages/(authenticated)/+Layout.tsx
+++ pages/(authenticated)/+Layout.tsx
@@ -87,6 +87,20 @@ export default function ({
       "https://emeka.prep50.com.ng",
       new Carbon(2024, 11, 1),
     );
+
+    orderService.registerSource(
+      "Nanyalove",
+      sourceUsingJambWaec("https://nanyalove.com.ng/api.php"),
+      "https://nanyalove.com.ng",
+      new Carbon(2024, 11, 1),
+    );
+
+    orderService.registerSource(
+      "Winbest",
+      sourceUsingJambWaec("https://www.winbestinternational.com.ng/api.php"),
+      "https://www.winbestinternational.com.ng",
+      new Carbon(2024, 11, 1),
+    );
     orderService.init().then((service) => {
       service.start();
     });
