6b3141a137d07c23012af6212de572f21fb23a86
epikoder <efedua.bell@gmail.com>
2025-07-20 20:45:30 +0100

    Remove https, fallback to nginx

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,28 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import vike from "vike/plugin";
-import fs from "fs";
+// import fs from "fs";
 
-let cert: string | undefined;
-let key: string | undefined;
-if (
-  fs.existsSync("./certs/cert.pem") && process.env.NODE_ENV != "development"
-) {
-  cert = fs.readFileSync("./certs/cert.pem", "utf8");
-  key = fs.readFileSync("./certs/private.pem", "utf8");
-}
+// let cert: string | undefined;
+// let key: string | undefined;
+// if (
+//   fs.existsSync("./certs/cert.pem") && process.env.NODE_ENV != "development"
+// ) {
+//   cert = fs.readFileSync("./certs/cert.pem", "utf8");
+//   key = fs.readFileSync("./certs/private.pem", "utf8");
+// }
 
 let pgrestURL = process.env.PG_REST ?? "http://localhost:3001";
 
 export default defineConfig({
   plugins: [vike({}), react({})],
   server: {
-    https: cert
-      ? {
-        cert,
-        key,
-      }
-      : undefined,
+    // https: cert
+    //   ? {
+    //     cert,
+    //     key,
+    //   }
+    //   : undefined,
     proxy: {
       "/rest": {
         target: pgrestURL,
