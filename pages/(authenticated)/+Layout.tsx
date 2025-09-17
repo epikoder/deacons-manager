d924fb1a5752be92708442174b2b65344e1801bb
epikoder <efedua.bell@gmail.com>
2025-09-17 15:42:05 +0100

    Added Ella Affiliate

--- pages/(authenticated)/+Layout.tsx
+++ pages/(authenticated)/+Layout.tsx
@@ -2,159 +2,156 @@ import React, { useEffect } from "react";
 import { Link } from "../../src/components/Link";
 import OrderService from "../../src/services/orders.service/orders.service";
 import {
-  sourceUsingJambWaec,
-  sourceUsingProductTable,
+	sourceUsingJambWaec,
+	sourceUsingProductTable,
 } from "../../src/services/orders.service/sources";
 import { usePageContext } from "vike-react/usePageContext";
 import Badge from "../../src/components/Badge";
 import Carbon from "../../src/utils/carbon";
 import Logo from "../../src/components/Logo";
 
-export default function ({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  const context = usePageContext();
+export default function ({ children }: { children: React.ReactNode }) {
+	const context = usePageContext();
 
-  if (!context.config.ready) return <></>;
+	if (!context.config.ready) return <></>;
 
-  useEffect(() => {
-    const orderService = OrderService.instance;
-    orderService.registerSource(
-      "Prep50Book",
-      sourceUsingJambWaec("https://prep50book.prep50mobileapp.com.ng/api.php"),
-      "https://prep50book.prep50mobileapp.com.ng",
-      new Carbon(2024, 11, 1),
-    );
+	useEffect(() => {
+		const orderService = OrderService.instance;
+		orderService.registerSource(
+			"Prep50Book",
+			sourceUsingJambWaec("https://prep50book.prep50mobileapp.com.ng/api.php"),
+			"https://prep50book.prep50mobileapp.com.ng",
+			new Carbon(2024, 11, 1),
+		);
 
-    orderService.registerSource(
-      "Prep50BookList",
-      sourceUsingJambWaec(
-        "https://prep50booklist.prep50mobileapp.com.ng/api.php",
-      ),
-      "https://prep50booklist.prep50mobileapp.com.ng",
-      new Carbon(2024, 11, 1),
-    );
+		orderService.registerSource(
+			"Prep50BookList",
+			sourceUsingJambWaec(
+				"https://prep50booklist.prep50mobileapp.com.ng/api.php",
+			),
+			"https://prep50booklist.prep50mobileapp.com.ng",
+			new Carbon(2024, 11, 1),
+		);
 
-    orderService.registerSource(
-      "Prep50BookSales",
-      sourceUsingJambWaec("https://prep50sales.prep50mobileapp.com.ng/api.php"),
-      "https://prep50sales.prep50mobileapp.com.ng",
-      new Carbon(2024, 11, 1),
-    );
+		orderService.registerSource(
+			"Prep50BookSales",
+			sourceUsingJambWaec("https://prep50sales.prep50mobileapp.com.ng/api.php"),
+			"https://prep50sales.prep50mobileapp.com.ng",
+			new Carbon(2024, 11, 1),
+		);
 
-    orderService.registerSource(
-      "Prep50BookList",
-      sourceUsingJambWaec(
-        "https://prep50booklist.prep50mobileapp.com.ng/api.php",
-      ),
-      "https://prep50booklist.prep50mobileapp.com.ng",
-      new Carbon(2024, 11, 1),
-    );
+		orderService.registerSource(
+			"Prep50BookList",
+			sourceUsingJambWaec(
+				"https://prep50booklist.prep50mobileapp.com.ng/api.php",
+			),
+			"https://prep50booklist.prep50mobileapp.com.ng",
+			new Carbon(2024, 11, 1),
+		);
 
-    orderService.registerSource(
-      "Nkemobi",
-      sourceUsingJambWaec("https://nkemobi.prep50mobileapp.com.ng/api.php"),
-      "https://nkemobi.prep50mobileapp.com.ng",
-      new Carbon(2024, 11, 1),
-    );
+		orderService.registerSource(
+			"Nkemobi",
+			sourceUsingJambWaec("https://nkemobi.prep50mobileapp.com.ng/api.php"),
+			"https://nkemobi.prep50mobileapp.com.ng",
+			new Carbon(2024, 11, 1),
+		);
 
-    orderService.registerSource(
-      "Mommacare",
-      sourceUsingJambWaec("https://mommacare.com.ng/api.php"),
-      "https://mommacare.com.ng",
-      new Carbon(2024, 11, 1),
-    );
+		orderService.registerSource(
+			"Mommacare",
+			sourceUsingJambWaec("https://mommacare.com.ng/api.php"),
+			"https://mommacare.com.ng",
+			new Carbon(2024, 11, 1),
+		);
 
-    orderService.registerSource(
-      "Arinze",
-      sourceUsingProductTable("https://arinze.prep50.com.ng/api/orders_v1"),
-      "https://arinze.prep50.com.ng",
-      new Carbon(2024, 11, 1),
-    );
+		orderService.registerSource(
+			"Arinze",
+			sourceUsingProductTable("https://arinze.prep50.com.ng/api/orders_v1"),
+			"https://arinze.prep50.com.ng",
+			new Carbon(2024, 11, 1),
+		);
 
-    orderService.registerSource(
-      "Nnacho",
-      sourceUsingProductTable("https://nnacho.prep50.com.ng/api/orders_v1"),
-      "https://nnacho.prep50.com.ng",
-      new Carbon(2024, 11, 1),
-    );
+		orderService.registerSource(
+			"Nnacho",
+			sourceUsingProductTable("https://nnacho.prep50.com.ng/api/orders_v1"),
+			"https://nnacho.prep50.com.ng",
+			new Carbon(2024, 11, 1),
+		);
 
-    orderService.registerSource(
-      "Emeka",
-      sourceUsingProductTable("https://emeka.prep50.com.ng/api/orders_v1"),
-      "https://emeka.prep50.com.ng",
-      new Carbon(2024, 11, 1),
-    );
+		orderService.registerSource(
+			"Emeka",
+			sourceUsingProductTable("https://emeka.prep50.com.ng/api/orders_v1"),
+			"https://emeka.prep50.com.ng",
+			new Carbon(2024, 11, 1),
+		);
 
-    orderService.registerSource(
-      "Nanyalove",
-      sourceUsingJambWaec("https://nanyalove.com.ng/api.php"),
-      "https://nanyalove.com.ng",
-      new Carbon(2025, 4, 1),
-    );
+		orderService.registerSource(
+			"Ella",
+			sourceUsingProductTable("https://ella.prep50.com.ng/api/orders_v1"),
+			"https://ella.prep50.com.ng",
+			new Carbon(2025, 8, 1),
+		);
 
-    orderService.registerSource(
-      "Winbest",
-      sourceUsingJambWaec("https://www.winbestinternational.com.ng/api.php"),
-      "https://www.winbestinternational.com.ng",
-      new Carbon(2025, 4, 1),
-    );
-    orderService.init().then((service) => {
-      service.start();
-    });
+		orderService.registerSource(
+			"Nanyalove",
+			sourceUsingJambWaec("https://nanyalove.com.ng/api.php"),
+			"https://nanyalove.com.ng",
+			new Carbon(2025, 4, 1),
+		);
 
-    return () => orderService.stopBackgroundPull();
-  }, []);
+		orderService.registerSource(
+			"Winbest",
+			sourceUsingJambWaec("https://www.winbestinternational.com.ng/api.php"),
+			"https://www.winbestinternational.com.ng",
+			new Carbon(2025, 4, 1),
+		);
+		orderService.init().then((service) => {
+			service.start();
+		});
 
-  return (
-    <div className="flex max-w-screen-xl mx-auto w-screen h-screen overflow-hidden">
-      <Sidebar>
-        <Logo />
-        <Link href="/">Dashboard</Link>
-        <Badge subscriber={OrderService.instance.notification}>
-          <Link href="/orders">Orders</Link>
-        </Badge>
-        <Link href="/affiliates">Affiliates</Link>
-        <Link href="/agents">Agents</Link>
-        {context.config.user!.role === "admin" &&
-          (
-            <Link href="/users">
-              Users
-            </Link>
-          )}
-        <Link href="/settings">Settings</Link>
-      </Sidebar>
-      <Content>{context.config.bookCost && children}</Content>
-    </div>
-  );
+		return () => orderService.stopBackgroundPull();
+	}, []);
+
+	return (
+		<div className="flex max-w-screen-xl mx-auto w-screen h-screen overflow-hidden">
+			<Sidebar>
+				<Logo />
+				<Link href="/">Dashboard</Link>
+				<Badge subscriber={OrderService.instance.notification}>
+					<Link href="/orders">Orders</Link>
+				</Badge>
+				<Link href="/affiliates">Affiliates</Link>
+				<Link href="/agents">Agents</Link>
+				{context.config.user!.role === "admin" && (
+					<Link href="/users">Users</Link>
+				)}
+				<Link href="/settings">Settings</Link>
+			</Sidebar>
+			<Content>{context.config.bookCost && children}</Content>
+		</div>
+	);
 }
 
 function Sidebar({ children }: { children: React.ReactNode }) {
-  return (
-    <div
-      id="sidebar"
-      className="p-5 flex flex-col gap-2 flex-shrink-0"
-      style={{
-        lineHeight: "1.8em",
-        borderRight: "2px solid #eee",
-      }}
-    >
-      {children}
-    </div>
-  );
+	return (
+		<div
+			id="sidebar"
+			className="p-5 flex flex-col gap-2 flex-shrink-0"
+			style={{
+				lineHeight: "1.8em",
+				borderRight: "2px solid #eee",
+			}}
+		>
+			{children}
+		</div>
+	);
 }
 
 function Content({ children }: { children: React.ReactNode }) {
-  return (
-    <div id="page-container" className="w-full">
-      <div
-        id="page-content"
-        className="h-screen overflow-hidden w-full"
-      >
-        {children}
-      </div>
-    </div>
-  );
+	return (
+		<div id="page-container" className="w-full">
+			<div id="page-content" className="h-screen overflow-hidden w-full">
+				{children}
+			</div>
+		</div>
+	);
 }
--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -2,99 +2,103 @@ import Carbon from "./carbon";
 import { postgrest, WithAuth } from "./postgrest";
 
 export {
-    cummalativeOrderByState,
-    cummalativeOrderForAffiliateByMonth,
-    formatQuery,
+	cummalativeOrderByState,
+	cummalativeOrderForAffiliateByMonth,
+	formatQuery,
 };
 
 const formatQuery = <T = Primitive>(
-    url: string,
-    params: T | undefined,
+	url: string,
+	params: T | undefined,
 ): string => {
-    if (!params) return encodeURI(url);
-    let query = "";
-    for (const k in params) {
-        if (typeof params[k] === "undefined" || params[k] == null) continue;
-        if (typeof params[k] === "object") {
-            for (const v of Object.values(params[k]!)) {
-                query += `&${k}=${v}`;
-            }
-        } else {
-            query += `&${k}=${
-                typeof params[k] === "boolean" ? (params[k] ? 1 : 0) : params[k]
-            }`;
-        }
-    }
-    query = encodeURI(`${url}?${query}`);
-    return query;
+	if (!params) return encodeURI(url);
+	let query = "";
+	for (const k in params) {
+		if (typeof params[k] === "undefined" || params[k] == null) continue;
+		if (typeof params[k] === "object") {
+			for (const v of Object.values(params[k]!)) {
+				query += `&${k}=${v}`;
+			}
+		} else {
+			query += `&${k}=${
+				typeof params[k] === "boolean" ? (params[k] ? 1 : 0) : params[k]
+			}`;
+		}
+	}
+	query = encodeURI(`${url}?${query}`);
+	return query;
 };
 
 const cummalativeOrderForAffiliateByMonth = async (year?: number) => {
-    const { data } = await new WithAuth(
-        postgrest.rpc("cummulative_orders_for_affiliate_by_month", {
-            year: year ?? new Date().getFullYear(),
-        }),
-    ).unwrap();
-    const result: Record<string, Record<string, Omit<MonthStats, "month">>> =
-        {};
-    for (const stat of (data ?? [])) {
-        const ms = result[stat.source] ?? {};
-        if (Object.values(ms).isEmpty()) {
-            Array.from(
-                Array(
-                    Carbon.lastMonthOfYear(year),
-                ).keys(),
-            ).map((month) =>
-                Carbon.fromYear(year ?? new Date().getFullYear(), month)
-                    .formatLocalISO()
-            ).forEach((month) =>
-                ms[month] = {
-                    delivered_count: 0,
-                    order_count: 0,
-                    pending_count: 0,
-                }
-            );
-        }
+	const { data } = (await new WithAuth(
+		postgrest.rpc("cummulative_orders_for_affiliate_by_month", {
+			year: year ?? new Date().getFullYear(),
+		}),
+	).unwrap()) as { data: any[] };
+	const result: Record<string, Record<string, Omit<MonthStats, "month">>> = {};
+	for (const stat of data ?? []) {
+		const ms = result[stat.source] ?? {};
+		if (Object.values(ms).isEmpty()) {
+			Array.from(Array(Carbon.lastMonthOfYear(year)).keys())
+				.map((month) =>
+					Carbon.fromYear(
+						year ?? new Date().getFullYear(),
+						month,
+					).formatLocalISO(),
+				)
+				.forEach(
+					(month) =>
+						(ms[month] = {
+							delivered_count: 0,
+							order_count: 0,
+							pending_count: 0,
+						}),
+				);
+		}
 
-        ms[stat.month] = {
-            delivered_count: stat.delivered_count,
-            order_count: stat.order_count,
-            pending_count: stat.pending_count,
-        };
-        result[stat.source] = ms;
-    }
-    return result;
+		ms[stat.month] = {
+			delivered_count: stat.delivered_count,
+			order_count: stat.order_count,
+			pending_count: stat.pending_count,
+		};
+		result[stat.source] = ms;
+	}
+	return result;
 };
 
 const cummalativeOrderByState = async (year?: number) => {
-    const { data } = await new WithAuth(
-        postgrest.rpc("cummulative_orders_by_state", {
-            year: year ?? new Date().getFullYear(),
-        }),
-    ).unwrap();
-    const result: Record<string, Record<string, Omit<MonthStats, "month">>> =
-        {};
-    for (const stat of (data ?? [])) {
-        const ms = result[stat.state] ?? {};
-        if (Object.values(ms).isEmpty()) {
-            Array.from(Array(12).keys()).map((month) =>
-                Carbon.fromYear(year ?? new Date().getFullYear(), month)
-                    .formatLocalISO()
-            ).forEach((month) =>
-                ms[month] = {
-                    delivered_count: 0,
-                    order_count: 0,
-                    pending_count: 0,
-                }
-            );
-        }
+	const { data } = await new WithAuth(
+		postgrest.rpc("cummulative_orders_by_state", {
+			year: year ?? new Date().getFullYear(),
+		}),
+	).unwrap();
+	const result: Record<string, Record<string, Omit<MonthStats, "month">>> = {};
+	for (const stat of data ?? []) {
+		const ms = result[stat.state] ?? {};
+		if (Object.values(ms).isEmpty()) {
+			Array.from(Array(12).keys())
+				.map((month) =>
+					Carbon.fromYear(
+						year ?? new Date().getFullYear(),
+						month,
+					).formatLocalISO(),
+				)
+				.forEach(
+					(month) =>
+						(ms[month] = {
+							delivered_count: 0,
+							order_count: 0,
+							pending_count: 0,
+						}),
+				);
+		}
 
-        ms[stat.month] = {
-            delivered_count: stat.delivered_count,
-            order_count: stat.order_count,
-            pending_count: stat.pending_count,
-        };
-        result[stat.state] = ms;
-    }
-    return result;
+		ms[stat.month] = {
+			delivered_count: stat.delivered_count,
+			order_count: stat.order_count,
+			pending_count: stat.pending_count,
+		};
+		result[stat.state] = ms;
+	}
+	return result;
 };
